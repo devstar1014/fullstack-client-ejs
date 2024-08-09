7c99bffccd72ab6c03385d88fee6ea1df36f598a
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-08-09 12:08:07 -0230

    Added CSS and fixed ejs objects

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { searchProducts } = require("../services/PG/p.fulltext.dal");
+const { checkAuthenticated } = require("../config/passport.config.js");
 
-router.get("/", async (req, res) => {
+router.get("/", checkAuthenticated, async (req, res) => {
   const query = req.query.q;
   if (!query) {
-    return res.render("search", { products: [], query: "" });
+    return res.render("search", { products: [], query: "", user: req.user });
   }
 
   try {
     const products = await searchProducts(query);
-    res.render("search", { products, query });
+    res.render("search", { products, query, user: req.user });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
